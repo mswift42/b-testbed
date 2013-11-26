bc563e3ffe9cbdc4e70f9cee9c722d8e8134010a
mswift42 <martin.haesler@gmail.com>
2013-11-26 16:00:35 +0000

    use regex in append function.

--- b-test.js
+++ b-test.js
@@ -2,9 +2,17 @@ function change_href(elt, hrefdest) {
     var oldhref = $(hrefdest).attr('href');
     // var mode = /flash[a-z0-9]*/.match($(elt).val());
     var str = "";
-    var mode = $(elt).val();
+    var mode = $(elt).attr("selected");//.match(/flash[a-z0-9]*/);
 	
     $(elt).change(function (){
 	$(hrefdest).attr('href',(oldhref + " " + mode));
 });
 };
+function man_href(elt,hrefdest) {
+    var oldhref = document.getElementById(hrefdest);
+    var hr = oldhref.innerHTML;
+    var e = document.getElementById(elt);
+    var strUser = e.options[e.selectedIndex].text;
+    hrefdest.innerHTML = oldhref + " " + strUser;
+    };
+    
